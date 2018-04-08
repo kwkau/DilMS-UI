695ff657d763d1d644b0f14e08717efe153c9df8
kwaku <kwekucsc@gmail.com>
2018-04-08 15:16:53 +0000

    Add project files.

---
+++ DilMS-UI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace DilMS_UI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ DilMS-UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DilMS_UI.WebApiApplication" Language="C#" %>
